ccfa356ad9be94016e0ae4b2cbf3da4f79d0c323
sbasinger <sbasinger@netjets.com>
2017-11-03 13:54:16 -0400

    MDSCV-359: Add lastUpdateDate to the refreshed token.

--- src/android/EventReceiver.java
+++ src/android/EventReceiver.java
@@ -14,6 +14,7 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.Date;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -129,6 +130,7 @@ public class EventReceiver extends BroadcastReceiver {
             }
             JSONObject newToken  = new RefreshTokenTask().execute(refreshUrl).get();
             System.out.println("New Token: "+newToken.toString(4));
+            newToken.lastUpdate = new Date().getTime();
             return newToken;
 
         } catch (Exception e) {
